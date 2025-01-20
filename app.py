74404218edc096774b0a16b73d0a25dd34d82a90
Eric Bash <eric.bash@burgessniple.com>
2025-01-20 10:56:11 -0500

    fw

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ def create_app():
     app = Quart(__name__)
     app.register_blueprint(bp)
     app.config["TEMPLATES_AUTO_RELOAD"] = True
-    app.config["PROVIDE_AUTOMATIC_OPTIONS"] = True
     return app
 
 
